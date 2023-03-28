cec75681a4157bdbe8c19c656f894485f3eca5a7
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2023-03-27 20:25:00 -0400

    Update no1.sh

--- no1.sh
+++ no1.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-# Define variables
-backup_dir="/user/hadoop/backup"
-source_dir="/user/hadoop/source"
-log_file="/home/hadoop/logs/backup.log"
+# Set the source and backup directories
+src_dir="/path/to/source/directory"
+backup_dir="/path/to/backup/directory"
 
-# Delete the backup folder if it exists
-hdfs dfs -test -d $backup_dir && hdfs dfs -rm -r $backup_dir
+# Set the log file path
+log_file="/path/to/log/file"
 
-# Copy the source folder to the backup folder
-hdfs dfs -cp -p $source_dir $backup_dir
+# Check if the backup directory already exists
+if hdfs dfs -test -d "${backup_dir}"; then
+  echo "$(date +"%Y-%m-%d %H:%M:%S"): Removing existing backup directory..." >> "${log_file}"
+  hdfs dfs -rm -r "${backup_dir}" >> "${log_file}" 2>&1
+fi
 
-# Validate that all files were copied successfully
-validation=$(hdfs dfs -diff $source_dir $backup_dir)
+# Create the backup directory
+echo "$(date +"%Y-%m-%d %H:%M:%S"): Creating backup directory..." >> "${log_file}"
+hdfs dfs -mkdir "${backup_dir}" >> "${log_file}" 2>&1
 
-if [[ $validation = "" ]]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Backup completed successfully." >> $log_file
+# Copy the files from the source directory to the backup directory
+echo "$(date +"%Y-%m-%d %H:%M:%S"): Copying files from source to backup directory..." >> "${log_file}"
+hdfs dfs -cp -p "${src_dir}/" "${backup_dir}/" >> "${log_file}" 2>&1
+
+# Validate the contents of the source and backup directories
+echo "$(date +"%Y-%m-%d %H:%M:%S"): Validating checksum of source directory..." >> "${log_file}"
+src_checksum=$(hdfs dfs -checksum "${src_dir}" | awk '{print $1}')
+backup_checksum=$(hdfs dfs -checksum "${backup_dir}" | awk '{print $1}')
+if [[ "${src_checksum}" == "${backup_checksum}" ]]; then
+  echo "$(date +"%Y-%m-%d %H:%M:%S"): Checksums match, backup is valid." >> "${log_file}"
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: Backup validation failed. Files were not copied successfully." >> $log_file
-    exit 1
+  echo "$(date +"%Y-%m-%d %H:%M:%S"): Checksums do not match, backup may be invalid." >> "${log_file}"
 fi
